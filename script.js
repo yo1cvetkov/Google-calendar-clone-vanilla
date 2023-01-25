c92eb2ea1370581f9b86e3dddee668c7a444ce4e
Jovan <yo1.cvetkov@gmail.com>
2023-01-25 20:34:32 +0100

    First and last commit

---
+++ script.js
@@ -0,0 +1,26 @@
+import { addMonths } from "date-fns";
+import renderMonth, { fixEventOverflow } from "./renderMonth.js";
+
+let selectedMonth = Date.now();
+document.querySelector("[data-next-month-btn]").addEventListener("click", () => {
+  selectedMonth = addMonths(selectedMonth, 1);
+  renderMonth(selectedMonth);
+});
+document.querySelector("[data-prev-month-btn]").addEventListener("click", () => {
+  selectedMonth = addMonths(selectedMonth, -1);
+  renderMonth(selectedMonth);
+});
+
+document.querySelector("[data-today-btn]").addEventListener("click", () => {
+  selectedMonth = Date.now();
+  renderMonth(selectedMonth);
+});
+let resizeTimeout;
+window.addEventListener("resize", () => {
+  if (resizeTimeout) clearTimeout(resizeTimeout);
+  resizeTimeout = setTimeout(() => {
+    document.querySelectorAll("[data-date-wrapper]").forEach(fixEventOverflow);
+  }, 100);
+});
+
+renderMonth(Date.now());
